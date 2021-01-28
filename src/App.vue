6b83f0ff2b278c1ea3d6a39872c3e361a006af00
Osi Emoekpere <osiloke@users.noreply.github.com>
2021-01-28 22:30:55 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        
+        
+        <div id="surveyElement">
+            <SurveyComponent />
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    import SurveyComponent from "./components/SurveyComponent";
+
+    export default {
+        name: "App",
+        components: {
+            SurveyComponent
+        }
+    };
+</script>
